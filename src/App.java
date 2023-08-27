25ad89e4b01a6e25f1c24c9541dd6444126b1e51
LuizSantosz <lgmsluiz@gmail.com>
2023-08-26 22:43:21 -0400

    aatividade 03

---
+++ src/App.java
@@ -0,0 +1,12 @@
+import java.time.LocalDate;
+
+import java.util.Scanner;
+
+import uniderp.poo.escola.dominio.*;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+
+}
+}
